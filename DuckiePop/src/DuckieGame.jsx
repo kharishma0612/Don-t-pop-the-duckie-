107d196c2c771f8e199a8bfcb481b3f06eabc21b
sharvaniSA <136978309+sharvaniSA@users.noreply.github.com>
2025-01-19 19:32:25 +0530

    main page game logic

--- DuckiePop/src/DuckieGame.jsx
+++ DuckiePop/src/DuckieGame.jsx
@@ -236,117 +236,267 @@
 // };
 
 // export default DuckieGame;
-import React from 'react';
+// import React from 'react';
+// import './DuckieGame.css'; // Import your CSS file
+
+// const DuckieGame = () => {
+//   return (
+//     <div className="desktop-1">
+//       {/* Background and layout elements */}
+//       <div className="rectangle-1"></div>
+//       <div className="ellipse-106"></div>
+//       <div className="ellipse-105"></div>
+//       <div className="line-1"></div>
+//       <div className="line-2"></div>
+//       <div className="line-3"></div>
+//       <div className="line-4"></div>
+//       <div className="line-6"></div>
+//       <div className="line-7"></div>
+//       <div className="rectangle-2"></div>
+//       <div className="rectangle-29"></div>
+//       <div className="rectangle-33"></div>
+      
+//       {/* Title Section */}
+//       <div className="title">
+//         <img className="titleimg" src="/icons/logo.png" alt="title" />
+//       </div>
+      
+//       {/* Hint, Eraser, and Bomb Icons */}
+//       <img className="hint" src="/icons/hint.ico" alt="Hint Icon" />
+//       <img className="eraser" src="/icons/eraser.ico" alt="Eraser Icon" />
+//       <img className="bomb" src="/icons/bomb.ico" alt="Bomb Icon" />
+      
+//       {/* Keyboard Layout */}
+//       <div className="keyboard">
+//         <div className="rectangle-3"></div>
+//         <div className="rectangle-4"></div>
+//         <div className="rectangle-5"></div>
+//         <div className="rectangle-6"></div>
+//         <div className="rectangle-7"></div>
+//         <div className="rectangle-8"></div>
+//         <div className="rectangle-9"></div>
+//         <div className="rectangle-10"></div>
+//         <div className="rectangle-11"></div>
+//         <div className="rectangle-12"></div>
+//         <div className="rectangle-13"></div>
+//         <div className="rectangle-14"></div>
+//         <div className="rectangle-15"></div>
+//         <div className="rectangle-16"></div>
+//         <div className="rectangle-17"></div>
+//         <div className="rectangle-18"></div>
+//         <div className="rectangle-19"></div>
+//         <div className="rectangle-20"></div>
+//         <div className="rectangle-21"></div>
+//         <div className="rectangle-22"></div>
+//         <div className="rectangle-23"></div>
+//         <div className="rectangle-24"></div>
+//         <div className="rectangle-25"></div>
+//         <div className="rectangle-26"></div>
+//         <div className="rectangle-27"></div>
+//         <div className="rectangle-28"></div>
+//       </div>
+
+//       {/* Alphabet Keys */}
+//       <div className="alphabet-keys">
+//         <div className="a">a</div>
+//         <div className="b">b</div>
+//         <div className="c">c</div>
+//         <div className="d">d</div>
+//         <div className="e">e</div>
+//         <div className="f">f</div>
+//         <div className="g">g</div>
+//         <div className="h">h</div>
+//         <div className="i">i</div>
+//         <div className="j">j</div>
+//         <div className="k">k</div>
+//         <div className="l">l</div>
+//         <div className="m">m</div>
+//         <div className="n">n</div>
+//         <div className="o">o</div>
+//         <div className="p">p</div>
+//         <div className="q">q</div>
+//         <div className="r">r</div>
+//         <div className="s">s</div>
+//         <div className="t">t</div>
+//         <div className="u">u</div>
+//         <div className="v">v</div>
+//         <div className="w">w</div>
+//         <div className="x">x</div>
+//         <div className="y">y</div>
+//         <div className="z">z</div>
+//       </div>
+
+//       {/* Game Stats */}
+//       <div className="stats">
+//         <div className="bulbs">2</div>
+//         <div className="bombs">3</div>
+//         <div className="erasers">2</div>
+//         <img className="coinsimg" src="/icons/coins.ico" alt="Coins Icon" />
+//         <div className="coins">420</div>
+//       </div>
+
+//       {/* Duck Image and Home Icon */}
+//       <img
+//         className="screenshot-2025-01-13-233312-1"
+//         src="icons/duck.png"
+//         alt="Duck Screenshot"
+//       />
+//       <img className="home-04" src="/icons/home.ico" alt="Home Icon" />
+//     </div>
+//   );
+// };
+
+// export default DuckieGame;
+import React, { useState, useEffect } from 'react';
 import './DuckieGame.css'; // Import your CSS file
 
+const words = ['javascript', 'react', 'hangman', 'duck', 'coding']; // Word list
 
 const DuckieGame = () => {
+  // Game States
+  const [targetWord, setTargetWord] = useState('');
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongGuesses, setWrongGuesses] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameState, setGameState] = useState('playing'); // 'playing', 'won', 'lost'
+  const [boosters, setBoosters] = useState(3);
+  const [hints, setHints] = useState(3);
+  const [undoCount, setUndoCount] = useState(3); // Added undo count state
+
+  useEffect(() => {
+    startNewGame();
+  }, []);
+
+  const startNewGame = () => {
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    setTargetWord(randomWord);
+    setGuessedLetters([]);
+    setWrongGuesses(0);
+    setScore(0);
+    setGameState('playing');
+    setBoosters(3);
+    setHints(3);
+    setUndoCount(3); // Reset undo count on new game
+  };
+
+  const handleLetterClick = (letter) => {
+    if (gameState !== 'playing' || guessedLetters.includes(letter)) return;
+
+    setGuessedLetters((prev) => [...prev, letter]);
+
+    if (targetWord.includes(letter)) {
+      setScore((prev) => prev + 10);
+    } else {
+      setWrongGuesses((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (wrongGuesses >= 4) {
+      setGameState('lost');
+    } else if (targetWord.split('').every((letter) => guessedLetters.includes(letter))) {
+      setGameState('won');
+    }
+  }, [guessedLetters, wrongGuesses, targetWord]);
+
+  const useHint = () => {
+    if (hints > 0 && gameState === 'playing') {
+      const hiddenLetters = targetWord.split('').filter((letter) => !guessedLetters.includes(letter));
+      if (hiddenLetters.length > 0) {
+        const randomHint = hiddenLetters[Math.floor(Math.random() * hiddenLetters.length)];
+        setGuessedLetters((prev) => [...prev, randomHint]);
+        setHints((prev) => prev - 1);
+      }
+    }
+  };
+
+  const useBooster = () => {
+    if (boosters > 0 && gameState === 'playing') {
+      const unusedLetters = 'abcdefghijklmnopqrstuvwxyz'
+        .split('')
+        .filter((letter) => !guessedLetters.includes(letter) && !targetWord.includes(letter));
+      if (unusedLetters.length > 0) {
+        const randomBooster = unusedLetters[Math.floor(Math.random() * unusedLetters.length)];
+        setGuessedLetters((prev) => [...prev, randomBooster]);
+        setBoosters((prev) => prev - 1);
+      }
+    }
+  };
+
+  const undoLastGuess = () => {
+    if (guessedLetters.length > 0 && gameState === 'playing' && undoCount > 0) {
+      const lastGuess = guessedLetters[guessedLetters.length - 1];
+      setGuessedLetters((prev) => prev.slice(0, -1));
+      if (!targetWord.includes(lastGuess)) {
+        setWrongGuesses((prev) => prev - 1);
+      }
+      setUndoCount((prev) => prev - 1); // Reduce undo count
+    }
+  };
+
+  const renderWord = () => {
+    return targetWord.split('').map((letter, index) => (
+      <span key={index} className="letter">
+        {guessedLetters.includes(letter) ? letter : '_'}
+      </span>
+    ));
+  };
+
+  const renderKeyboard = () => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return alphabet.split('').map((letter) => (
+      <button
+        key={letter}
+        className="keyboard-key"
+        disabled={guessedLetters.includes(letter) || gameState !== 'playing'}
+        onClick={() => handleLetterClick(letter)}
+      >
+        {letter}
+      </button>
+    ));
+  };
+
   return (
     <div className="desktop-1">
-      <div className="rectangle-1"></div>
-      <div className="ellipse-106"></div>
-      <div className="ellipse-105"></div>
-      <div className="line-2"></div>
-      <div className="line-3"></div>
-      <div className="line-4"></div>
-      <div className="line-7"></div>
-      <div className="line-6"></div>
-      <div className="line-1"></div>
-      <div className="rectangle-2"></div>
-      <div className="rectangle-29"></div>
-      <img
-        className="_32-officeicons-1-89729-3"
-        src="/icons/hint.ico"
-        alt="Office Icons"
-      />
-      <div className="frame-1"></div>
-      <div className="title">
-      <img
-        className="titleimg"
-        src="/icons/logo.png"
-        alt="title"
-      />
+      {/* Left Side for Duckie */}
+      <div className="duckie-pool">
+        <img className="duckie-image" src="/icons/duck.png" alt="Duckie" />
       </div>
-      <div className="rectangle-3"></div>
-      <div className="rectangle-4"></div>
-      <div className="rectangle-5"></div>
-      <div className="rectangle-24"></div>
-      <div className="rectangle-6"></div>
-      <div className="rectangle-25"></div>
-      <div className="rectangle-7"></div>
-      <div className="d">d</div>
-      <div className="c">c</div>
-      <div className="rectangle-26"></div>
-      <div className="rectangle-8"></div>
-      <div className="rectangle-27"></div>
-      <div className="rectangle-9"></div>
-      <div className="rectangle-28"></div>
-      <div className="rectangle-10"></div>
-      <div className="rectangle-17"></div>
-      <div className="rectangle-11"></div>
-      <div className="rectangle-18"></div>
-      <div className="rectangle-12"></div>
-      <div className="rectangle-19"></div>
-      <div className="rectangle-13"></div>
-      <div className="rectangle-20"></div>
-      <div className="rectangle-14"></div>
-      <div className="rectangle-21"></div>
-      <div className="rectangle-15"></div>
-      <div className="rectangle-22"></div>
-      <div className="rectangle-16"></div>
-      <div className="rectangle-23"></div>
-      <div className="a">a</div>
-      <div className="b">b</div>
-      <div className="e">e</div>
-      <div className="f">f</div>
-      <div className="g">g</div>
-      <div className="h">h</div>
-      <div className="i">i</div>
-      <div className="j">j</div>
-      <div className="k">k</div>
-      <div className="l">l</div>
-      <div className="m">m</div>
-      <div className="n">n</div>
-      <div className="o">o</div>
-      <div className="p">p</div>
-      <div className="q">q</div>
-      <div className="r">r</div>
-      <div className="s">s</div>
-      <div className="t">t</div>
-      <div className="u">u</div>
-      <div className="v">v</div>
-      <div className="w">w</div>
-      <div className="x">x</div>
-      <div className="y">y</div>
-      <div className="z">z</div>
-      <div className="bulbs">2</div>
-      <div className="bombs">3</div>
-      <div className="erasers">2</div>
-      <div className="rectangle-33"></div>
-      <img className="coinsimg" src="/icons/coins.ico" alt="coins" />
-      
-      <div className="coins">420</div>
-      {/* <img className="line-8" src="line-80.svg" alt="Line 8" /> */}
-      {/* <div className="line-9"></div> */}
-      {/* <img className="ellipse-104" src="ellipse-1040.svg" alt="Ellipse 104" /> */}
-      <img
-        className="screenshot-2025-01-13-233312-1"
-        src="icons/duck.png"
-        alt="Screenshot"
-      />
-      <img
-        className="gui-eraser-icon-157160-2"
-        src="/icons/eraser.ico"
-        alt="Eraser Icon"
-      />
-      <img
-        className="console-gamepad-bomb-sports-gaming-video-sport-game-controller-icon-262437-3"
-        src="/icons/bomb.ico"
-        alt="Gamepad Icon"
-      />
-      <img className="home-04" src="/icons/home.ico" alt="Home Icon" />
+
+      {/* Right Side for Game Content */}
+      <div className="game-container">
+        <div className="header">
+          <div className="score">Score: {score}</div>
+          <div className="boosters">Boosters: {boosters}</div>
+          <div className="hints">Hints: {hints}</div>
+          <div className="wrong-guesses">moves: {4 - wrongGuesses}</div> {/* Display remaining wrong guesses */}
+          <div className="undo-count">Undo: {undoCount}</div> {/* Display undo count */}
+        </div>
+        <div className="word-display">{renderWord()}</div>
+        <div className="keyboard">{renderKeyboard()}</div>
+        <div className="actions">
+          <button className="action-button" onClick={useHint} disabled={hints <= 0}>
+            Use Hint
+          </button>
+          <button className="action-button" onClick={useBooster} disabled={boosters <= 0}>
+            Use Booster
+          </button>
+          <button className="action-button" onClick={undoLastGuess} disabled={undoCount <= 0 || guessedLetters.length === 0}>
+            Undo Guess
+          </button>
+        </div>
+        {gameState !== 'playing' && <div className="game-message">{gameState === 'won' ? 'You Win!' : 'You Lose!'}</div>}
+        
+        {/* Restart Button */}
+        <div className="restart-container">
+          <button className="restart-button" onClick={startNewGame}>
+            Restart Game
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default DuckieGame;
+
